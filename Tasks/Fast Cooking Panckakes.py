de2528b1cd45605805c9480cc4f0073c5f16c1aa
Your Name <mau@em.com>
2024-04-03 16:37:19 +0300

    codewars task

---
+++ Tasks/Fast Cooking Panckakes.py
@@ -0,0 +1,5 @@
+from math import ceil
+def cook_pancakes(n, m):
+    return ceil(n/m)*2
+import string
+print(string.ascii_lowercase)
---
+++ Tasks/Is it a letter.py
@@ -0,0 +1,6 @@
+import string
+def is_it_letter(s):
+    if s in string.ascii_letters:
+        return True
+    else:
+        return False
